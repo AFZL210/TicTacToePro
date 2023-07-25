4c5d72f0b5e47f60e97c242641172a3c765f1407
AFZL210 <tricktomade@gmail.com>
2023-07-25 22:36:08 +0530

    added home page, ui setup

--- game/src/components/Button.jsx
+++ game/src/components/Button.jsx
@@ -3,7 +3,10 @@ import P from "../assets/pvp.png"
 
 const Button = (props) => {
   return (
-        <button className='w-[12rem] h-[1.8rem] bg-white flex items-center px-3 justify-around' onClick={() => props.toggle(props.togleOption)} >
+        <button className='w-[12rem] h-[1.8rem] bg-white flex items-center px-3 justify-around' onClick={() => {
+          if(props.type=="toggle") props.toggle(props.togleOption);
+          else props.onClickHandler();
+        }} >
             <img className='w-[1.4rem]' src={P}/>
             <span className='text-[#000] w-[10.6rem]'>{props.title}</span>
         </button>
--- game/src/pages/Home.jsx
+++ game/src/pages/Home.jsx
@@ -9,22 +9,20 @@ const Home = () => {
     const toggleDialogBox = (box) => setDialogBox(box);
     const [username, setUsername] = useState("");
     const [username2, setUsername2] = useState("");
-    const [roomId, setRoomId] = useState(null);
-
-    console.log(username)
+    const [roomId, setRoomId] = useState("");
 
     return (
         <div className='w-[100vw] h-[100vh] bg-[#1e1e20] flex items-center justify-center'>
             <div className='w-[50vw] h-[50vh] flex flex-col relative'>
                 <h1 className='mx-auto text-2xl font-bold'>TicTacToePro</h1>
                 <div className='w-[100%] h-[100%] flex flex-col items-center gap-5 mt-6'>
-                    <Button title="Player vs Player" toggle={toggleDialogBox} togleOption="pvp" />
-                    <Button title="Player vs Bot" toggle={toggleDialogBox} togleOption="pvb" />
-                    <Button title="Watch Bot Fight" toggle={toggleDialogBox} togleOption="bvb" />
+                    <Button title="Player vs Player" toggle={toggleDialogBox} togleOption="pvp" type="toggle" />
+                    <Button title="Player vs Bot" toggle={toggleDialogBox} togleOption="pvb" type="toggle" />
+                    <Button title="Watch Bot Fight" toggle={toggleDialogBox} togleOption="bvb" type="toggle" />
                 </div>
                 {dialogBox !== null && <div className='absolute w-[100%] h-[100%] top-0 bg-black flex flex-col'>
                     <div className='w-[100%] text-right px-4 py-4 '><span onClick={() => setDialogBox(null)} className='w-[2.2rem] cursor-pointer text-[#fff] text-[1.2rem] rounded-full'>X</span></div>
-                    {dialogBox == "pvp" ? <JoinCreateRoom username={username} setUsername={setUsername} roomId={roomId} setRoomId={setRoomId} />
+                    {dialogBox == "pvp" ? <JoinCreateRoom username={username} setUsername={setUsername} roomId={roomId} setRoomId={setRoomId} username2={username2} setUsername2={setUsername2} />
                         : dialogBox == "bvb" ? <BotVsBot />
                             : <PlayerVsBot />}</div>}
             </div>
@@ -34,6 +32,7 @@ const Home = () => {
 
 
 const JoinCreateRoom = (props) => {
+
     return (
         <div className='w-[100%] flex flex-col items-center'>
             <h1>Join Room</h1>
